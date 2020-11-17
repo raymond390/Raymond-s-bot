99c79e5517dff2009f56f946b4b35cdc8c25de18
raymond390 <74500294+raymond390@users.noreply.github.com>
2020-11-17 18:02:26 +0100

    Create unban.js

---
+++ commands/unban.js
@@ -0,0 +1,21 @@
+const discord = require("discord.js");
+
+module.exports.run = async (client, message, argument) => {
+    if(command === "!unban")
+        if(!msg.member.hasPermission("BAN_MEMBERS")) {
+          return msg.channel.send(`**${msg.author.username}**, You do not have perms to unban someone`)
+        }
+        
+        if(!msg.guild.me.hasPermission("BAN_MEMBERS")) {
+          return msg.channel.send(`**${msg.author.username}**, I do not have perms to unban someone`)
+        }
+        
+        let userID = args[0]
+          msg.guild.fetchBans().then(bans=> {
+          if(bans.size == 0) return 
+          let bUser = bans.find(b => b.user.id == userID)
+          if(!bUser) return
+          msg.guild.members.unban(bUser.user)
+    })
+        
+      ;}
