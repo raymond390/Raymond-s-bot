2d75091c18f3e85ea5cc1791fa624f7791817b2d
raymond390 <74500294+raymond390@users.noreply.github.com>
2020-11-17 17:45:21 +0100

    Update review.js

--- commands/review.js
+++ commands/review.js
@@ -10,7 +10,7 @@ module.exports.run = async (client, message, args) => {
 
     var text = args.splice(1, args.length).join(" ") || '**Geen tekst opgegeven**';
 
-    var channel = message.member.guild.channels.cache.get("581753986839871498");
+    var channel = message.member.guild.channels.cache.get("760836476102639651");
 
     if (!channel) return message.channel.send("Kanaal bestaat niet");
 
