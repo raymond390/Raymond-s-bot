142472e5d8276341437b811f7d0365c71ea0659c
raymond390 <74500294+raymond390@users.noreply.github.com>
2020-11-17 19:33:35 +0100

    Update claim.js

--- commands/claim.js
+++ commands/claim.js
@@ -4,18 +4,21 @@ module.exports.run = async (client, message, args) => {
 
     const categoryID = "761879667124600842";
 
+    if (!message.member.hasPermission("KICK_MEMBER")) return message.reply("Jij kan dit niet doen");
 
     if (message.channel.parentID == categoryID) {
 
-        
+        message.channel.delete();
+
+        // Create embed.
+        var embedCreateTicket = new discord.MessageEmbed()
+            .setTitle("Ticket, " + message.channel.name)
+            .setDescription("Het ticket is gemarkeerd als **compleet**.")
+            .setFooter("Ticket gesloten");
 
         // Channel voor logging
-   
+        var ticketChannel = message.member.guild.channels.cache.find(channel => channel.name === "log");
         if (!ticketChannel) return message.reply("Kanaal bestaat niet");
-        var embedCreateTicket = new discord.MessageEmbed()
-        .setTitle("Ticket, " + message.channel.name)
-        .setDescription("Het ticket is gemarkeerd als **compleet**.")
-        .setFooter("Ticket gesloten")
 
         ticketChannel.send(embedCreateTicket);
 
