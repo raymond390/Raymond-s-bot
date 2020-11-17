8a41ed374a728aef0ed4d862bd883d97eaaa0545
raymond390 <74500294+raymond390@users.noreply.github.com>
2020-11-17 17:04:29 +0100

    test

--- commands/info.js
+++ commands/info.js
@@ -6,7 +6,7 @@ module.exports.run = async (client, message, args) => {
             .setTitle('Defensie')
             .setDescription("werken bij defensie je moet het maar kunnen")
             .setColor("#0099ff")
-            .setDescription("Ga werken bij defensie je moet het maar kunnen")
+            .setDescription("werken bij defensie je moet het maar kunnen")
 
             .setThumbnail('https://yt3.ggpht.com/a/AGF-l7_P42jUnWAoIvDDHzQH27EIOs5hAoDAwfdgug=s900-c-k-c0xffffffff-no-rj-mo')
             .setImage('https://f.jwwb.nl/public/g/w/t/temp-mxahzxdricxvqfpwpnhx/lm5nsv/km.jpg')
