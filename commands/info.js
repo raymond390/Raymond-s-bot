d15120ccc2be5fb42dfe58778fddf0300ac56d8e
raymond390 <74500294+raymond390@users.noreply.github.com>
2020-11-17 17:20:34 +0100

    Update info.js

--- commands/info.js
+++ commands/info.js
@@ -7,8 +7,8 @@ module.exports.run = async (client, message, args) => {
             .setColor("#0099ff")
             .setDescription("Ik ben aan het leren")
 
-            .setThumbnail('https://yt3.ggpht.com/a/AGF-l7_P42jUnWAoIvDDHzQH27EIOs5hAoDAwfdgug=s900-c-k-c0xffffffff-no-rj-mo')
-            .setImage('https://f.jwwb.nl/public/g/w/t/temp-mxahzxdricxvqfpwpnhx/lm5nsv/km.jpg')
+            .setThumbnail('')
+            .setImage('')
             .setTimestamp()
             .setFooter('test', '');
 
