0333fa1b2224121d98e39eced47dcd7b7c7054af
raymond390 <74500294+raymond390@users.noreply.github.com>
2020-11-18 15:17:17 +0100

    Update tempMute.js

--- commands/tempMute.js
+++ commands/tempMute.js
@@ -17,7 +17,7 @@ module.exports.run = async (client, message, args) => {
 
     if (mutePerson.hasPermission("MANAGE_MESSAGES")) return message.reply("Sorry je kunt deze gebruiker niet muten");
 
-    var muteRole = message.guild.roles.cache.get('461925992383119390');
+    var muteRole = message.guild.roles.cache.get('761247249727619093');
     if (!muteRole) return message.channel.send("De rol muted bestaat niet.");
 
     var muteTime = args[1];
