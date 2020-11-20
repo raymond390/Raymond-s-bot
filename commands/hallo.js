302f41f8e29b294e231b39eb56c052de5eedcc88
raymond390 <74500294+raymond390@users.noreply.github.com>
2020-11-20 20:27:21 +0100

    Update hallo.js

--- commands/hallo.js
+++ commands/hallo.js
@@ -2,7 +2,29 @@ const discord = require("discord.js");
 
 module.exports.run = async (client, message, args) => {
 
-    return message.channel.send("Hallo!!");
+    var item = "";
+    var time;
+    var winnerCount;
+
+    if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("Sorry jij kan dit niet doen");
+
+    winnerCount = args[0];
+    time = args[1];
+    item = args.splice(2, args.length).join(" ");
+
+    if (!winnerCount) return message.reply("Geen aantal spelers opgegeven");
+    if (!time) return message.reply("Geen tijd opgegeven");
+    if (!item) return message.reply("Geen winnaars item opgegeven");
+
+    message.delete();
+
+    var date = new Date().getTime();
+    var dateEnd = new Date(date + (time * 1000));
+
+    var giveawayEmbed = new discord.MessageEmbed()
+        .setTitle("🎉 **GIVEAWAY** 🎉")
+        .setFooter(`Vervalt: ${dateEnd}`)
+        .setDescription(item);
 
 }
 
