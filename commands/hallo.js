8f6a1c84a3879ea6fa337fa951b9ee770750ccf6
raymond390 <74500294+raymond390@users.noreply.github.com>
2020-11-20 20:20:18 +0100

    Update hallo.js

--- commands/hallo.js
+++ commands/hallo.js
@@ -2,9 +2,10 @@ const discord = require("discord.js");
 
 module.exports.run = async (client, message, args) => {
 
+    var kickUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
     var embed = new discord.MessageEmbed()
         .setColor("#ff0000")
-        .setThumbnail(user.displayAvatarURL)
+        .setThumbnail(kickUser.user.displayAvatarURL)
         .setFooter(message.member.displayName, message.author.displayAvatarURL)
         .setTimestamp()
         .setDescription(`** Gekickt:** )
