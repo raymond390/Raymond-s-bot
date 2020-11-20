cd5eacd5dfb25f4dfe69c70b3a95b3ff3a4ae8a0
raymond390 <74500294+raymond390@users.noreply.github.com>
2020-11-20 20:08:18 +0100

    Update hallo.js

--- commands/hallo.js
+++ commands/hallo.js
@@ -2,12 +2,18 @@ const discord = require("discord.js");
 
 module.exports.run = async (client, message, args) => {
 
-    return message.channel.send("Hallo!!");
-
+    var embed = new discord.MessageEmbed()
+    .setColor("#ff0000")
+    .setFooter(message.member.displayName, message.author.displayAvatarURL)
+    .setTimestamp()
+    .setDescription(`**Gewarnd:** ${warnUser} (${warnUser.id})
+    **Warning door:** ${message.author}
+    **Redenen: ** ${reason}`)
+    .addField("Aantal warns", warns[warnUser.id].warns);
 }
 
 module.exports.help = {
-    name: "hallo",
+    name: "test",
     description: "Geeft al de verschillende commands",
     category: "Informatie"
 }
