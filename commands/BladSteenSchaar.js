90bfd2206687c94ba297caa7610ea2c756d0d702
raymond390 <74500294+raymond390@users.noreply.github.com>
2020-11-21 00:02:06 +0100

    Update BladSteenSchaar.js

--- commands/BladSteenSchaar.js
+++ commands/BladSteenSchaar.js
@@ -65,6 +65,6 @@ module.exports.run = async (client, message, args) => {
 
 module.exports.help = {
     name: "oorlog",
-    description: "",
-    category: ""
+    description: "doe .oorlog colt,sniper,clock",
+    category: "Informatie"
 }
