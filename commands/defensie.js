fde6dad234b46bbced4bb5a022f94428599545af
raymond390 <74500294+raymond390@users.noreply.github.com>
2020-11-23 19:46:55 +0100

    Update defensie.js

--- commands/defensie.js
+++ commands/defensie.js
@@ -1,12 +1,13 @@
 const discord = require("discord.js");
 
 module.exports.run = async (client, message, args) => {
+
     var reason = args.slice(1).join(" ");
     
     var botEmbed = new discord.MessageEmbed()
             .setTitle('Defensie')
             .setColor("#0099ff")
-            .setDescription(`Hoi test
+            .setDescription(`Hoi 
             **Redenen: ** ${reason}`)
 
             .setThumbnail('https://yt3.ggpht.com/a/AGF-l7_P42jUnWAoIvDDHzQH27EIOs5hAoDAwfdgug=s900-c-k-c0xffffffff-no-rj-mo')
