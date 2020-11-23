6769c337635b65cb213970f7f8a9fa419eab1424
raymond390 <74500294+raymond390@users.noreply.github.com>
2020-11-23 20:19:18 +0100

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -4,12 +4,15 @@ module.exports.run = async (client, message, args) => {
     var reason = args.slice(1).join(" ");
     
     var botEmbed = new discord.MessageEmbed()
-        .setColor("#ff0000")
-        .setFooter()
-        .setTimestamp()
-        .setDescription(` **Redenen: ** ${reason}`)
-        .addField()
-
+    .setTitle('Defensie')
+    .setColor("#0099ff")
+    .setDescription(`
+    **Redenen: ** ${reason}`)
+    .addField("Aantal warns", warns[warnUser.id].warns)
+    .setThumbnail('https://yt3.ggpht.com/a/AGF-l7_P42jUnWAoIvDDHzQH27EIOs5hAoDAwfdgug=s900-c-k-c0xffffffff-no-rj-mo')
+    .setImage('https://f.jwwb.nl/public/g/w/t/temp-mxahzxdricxvqfpwpnhx/lm5nsv/km.jpg')
+    .setTimestamp()
+    .setFooter('Defensie', '');
 }
 
 module.exports.help = {
