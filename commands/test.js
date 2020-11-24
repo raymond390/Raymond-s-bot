f2920cd5ac201a623be7faef89c68b5b5bf93e30
raymond390 <74500294+raymond390@users.noreply.github.com>
2020-11-24 17:08:37 +0100

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -1,22 +1,22 @@
 const discord = require("discord.js");
 
 module.exports.run = async (client, message, args) => {
-    var dienst = args.slice(0).join(" ");
-
+    var test = args.slice(0).join(" ");
     var time = args.slice(2).join(" ");
-
     var type = args.slice(1).join(" ");
-
     var a = args.slice(3).join(" ");
 
     var botEmbed = new discord.MessageEmbed()
     .setTitle('Defensie')
     .setColor("#0099ff")
+    .setTimestamp()
     .setDescription(`
-    ** Dienst **  ${dienst},
-    ** type-Training ** ${type},
-    ** Host ** ${time}`)
+    ** Training ** ${test}`)
+    .addFields(
+        {name:" ** Training **" , value:'${test}' },
+    )
     .setThumbnail('https://yt3.ggpht.com/a/AGF-l7_P42jUnWAoIvDDHzQH27EIOs5hAoDAwfdgug=s900-c-k-c0xffffffff-no-rj-mo')
+    .setImage('https://f.jwwb.nl/public/g/w/t/temp-mxahzxdricxvqfpwpnhx/lm5nsv/km.jpg')
     .setFooter('Defensie', '');
 
 
@@ -24,7 +24,7 @@ module.exports.run = async (client, message, args) => {
 }
 
 module.exports.help = {
-    name: "training",
+    name: "test",
     description: "",
     category: "Algemeen"
 }
