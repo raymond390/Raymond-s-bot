fa484dc42e9c5ad3e5ea01629b18f3f0aa5ee129
raymond390 <74500294+raymond390@users.noreply.github.com>
2020-11-24 17:46:44 +0100

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -2,7 +2,7 @@ const discord = require("discord.js");
 
 module.exports.run = async (client, message, args) => {
     var test = args.slice(0).join(" ");
-    var time = args.slice(2).join(" ");
+    var dienst = args.slice(2).join(" ");
     var type = args.slice(1).join(" ");
     var a = args.slice(3).join(" ");
 
@@ -13,7 +13,11 @@ module.exports.run = async (client, message, args) => {
     .setDescription(`
     ** Training ** ${test}`)
     .addFields(
-        {name:" ** Training **" , value:` '${test} `}
+        {name:" ** Training **" , value:` ${test} `},
+        {name:" ** Dienst **" , value:` ${dienst} `},
+        {name:" ** Type-Training **" , value:` ${type} `},
+        {name:" ** Host **" , value:` ${message.author} `}
+
     )
     .setThumbnail('https://yt3.ggpht.com/a/AGF-l7_P42jUnWAoIvDDHzQH27EIOs5hAoDAwfdgug=s900-c-k-c0xffffffff-no-rj-mo')
     .setImage('https://f.jwwb.nl/public/g/w/t/temp-mxahzxdricxvqfpwpnhx/lm5nsv/km.jpg')
