84ea34a94b97197aa45c1d6efec48a0667919988
raymond390 <74500294+raymond390@users.noreply.github.com>
2020-11-24 17:13:40 +0100

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -10,11 +10,8 @@ module.exports.run = async (client, message, args) => {
     .setTitle('Defensie')
     .setColor("#0099ff")
     .setTimestamp()
-    .setDescription(`
-    ** Training ** ${test}`)
     .addFields(
-        {name:" ** Training **" , value:'${test}' },
-    )
+        {name:" ** Training ** " , value: "${test}" })
     .setThumbnail('https://yt3.ggpht.com/a/AGF-l7_P42jUnWAoIvDDHzQH27EIOs5hAoDAwfdgug=s900-c-k-c0xffffffff-no-rj-mo')
     .setImage('https://f.jwwb.nl/public/g/w/t/temp-mxahzxdricxvqfpwpnhx/lm5nsv/km.jpg')
     .setFooter('Defensie', '');
