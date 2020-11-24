9b7ceb3868354baaa08b60b038bfd7de2e82ef76
raymond390 <74500294+raymond390@users.noreply.github.com>
2020-11-24 16:51:54 +0100

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -6,8 +6,8 @@ module.exports.run = async (client, message, args) => {
     var time = args.slice(2).join(" ");
 
     var type = args.slice(1).join(" ");
-    
-    var a = args.slice(0).join(" ");
+
+    var a = args.slice(4).join(" ");
 
     var botEmbed = new discord.MessageEmbed()
     .setTitle('Defensie')
@@ -16,9 +16,6 @@ module.exports.run = async (client, message, args) => {
     ** Dienst **  ${dienst}
     ** type-Training ** ${type}
     ** Host ** ${time}`)
-    
-    .setThumbnail('https://yt3.ggpht.com/a/AGF-l7_P42jUnWAoIvDDHzQH27EIOs5hAoDAwfdgug=s900-c-k-c0xffffffff-no-rj-mo')
-    .setImage('https://f.jwwb.nl/public/g/w/t/temp-mxahzxdricxvqfpwpnhx/lm5nsv/km.jpg')
     .setFooter('Defensie', '');
 
 
@@ -26,7 +23,7 @@ module.exports.run = async (client, message, args) => {
 }
 
 module.exports.help = {
-    name: "test",
+    name: "training",
     description: "",
     category: "Algemeen"
 }
