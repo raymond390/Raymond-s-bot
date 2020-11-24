39b423629ada3cdb313e655296d411ab3e3c8ff5
raymond390 <74500294+raymond390@users.noreply.github.com>
2020-11-24 16:34:53 +0100

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -1,17 +1,22 @@
 const discord = require("discord.js");
 
 module.exports.run = async (client, message, args) => {
-    var test = args.slice(0).join(" ");
-    var time = args.slice(1).join(" ");
-    var type = args.slice(0).join(" ");
+    var dienst = args.slice(0).join(" ");
+    var time = args.slice(3).join(" ");
+    var type = args.slice(1).join(" ");
     var a = args.slice(0).join(" ");
 
     var botEmbed = new discord.MessageEmbed()
     .setTitle('Defensie')
     .setColor("#0099ff")
     .setDescription(`
-    ** Training ** ${test})
-    ** tijd ** ${time})`)
+    ** Dienst **
+     ${dienst})
+    ** type-Training ** 
+    ${type}
+    ** Host ** 
+    ${time}`)
+    
     .setThumbnail('https://yt3.ggpht.com/a/AGF-l7_P42jUnWAoIvDDHzQH27EIOs5hAoDAwfdgug=s900-c-k-c0xffffffff-no-rj-mo')
     .setImage('https://f.jwwb.nl/public/g/w/t/temp-mxahzxdricxvqfpwpnhx/lm5nsv/km.jpg')
     .setFooter('Defensie', '');
