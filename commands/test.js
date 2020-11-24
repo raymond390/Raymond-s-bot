180d22784101e8363c6335b80eb07ac01085572d
raymond390 <74500294+raymond390@users.noreply.github.com>
2020-11-24 17:25:29 +0100

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -11,10 +11,10 @@ module.exports.run = async (client, message, args) => {
     .setColor("#0099ff")
     .setTimestamp()
     .addFields(
-        {name:" ** Training ** " , value: "'${test}'" },
+        {name:" ** Training ** " , value: {test} },
         {name:" ** Training ** " , value: "${time}" },
         {name:" ** Training ** " , value: "${type}" },
-        {name:" ** Training ** " , value: "${}" }
+        {name:" ** Training ** " , value: '' }
     )
     .setThumbnail('https://yt3.ggpht.com/a/AGF-l7_P42jUnWAoIvDDHzQH27EIOs5hAoDAwfdgug=s900-c-k-c0xffffffff-no-rj-mo')
     .setImage('https://f.jwwb.nl/public/g/w/t/temp-mxahzxdricxvqfpwpnhx/lm5nsv/km.jpg')
