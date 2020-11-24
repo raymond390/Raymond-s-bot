63964299118c4077e622e02070233029ba5ecdfd
raymond390 <74500294+raymond390@users.noreply.github.com>
2020-11-24 16:13:26 +0100

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -8,10 +8,8 @@ module.exports.run = async (client, message, args) => {
     .setColor("#0099ff")
     .setDescription(`
     **Redenen: ** ${test}`)
-    .addField("")
     .setThumbnail('https://yt3.ggpht.com/a/AGF-l7_P42jUnWAoIvDDHzQH27EIOs5hAoDAwfdgug=s900-c-k-c0xffffffff-no-rj-mo')
     .setImage('https://f.jwwb.nl/public/g/w/t/temp-mxahzxdricxvqfpwpnhx/lm5nsv/km.jpg')
-    .setTimestamp()
     .setFooter('Defensie', '');
 
 
