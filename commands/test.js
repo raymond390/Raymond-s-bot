d0b7c44142e09a5af2750c98e99c6ae136443a52
raymond390 <74500294+raymond390@users.noreply.github.com>
2020-11-25 15:58:35 +0100

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -2,9 +2,9 @@ const discord = require("discord.js");
 
 module.exports.run = async (client, message, args) => {
     var test = args.slice(0).join(" ");
-    var dienst = args.slice(0).join(" ");
-    var type = args.slice(0).join(" ");
-    var a = args.slice(0).join(" ");
+    var dienst = args.slice(1).join(" ");
+    var type = args.slice(2).join(" ");
+    var a = args.slice(3).join(" ");
     if (!args[0]) return message.reply("Geen gebruiker opgegeven.");
 
     if (!args[1]) return message.reply("Gelieve een redenen op te geven.");
